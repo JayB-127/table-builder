1f9552d369819a5d61336dc1c637cdf012274755
Jacob <jw.braidley@gmail.com>
2024-08-31 00:20:00 +0100

    readability changes + TODOs

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from typing import Self
 
 
 class Table:
@@ -6,19 +7,22 @@ class Table:
         self.rows = rows
         self.colNo = len(cols)
         self.rowNo = len(rows)
+        
 
-    def build(self):
-        # raise TypeError if not lists?
-        # handle unmatching numbers of cols to rows
+    def show(self) -> Self:
 
-        colLens = []
-        # append longest value length for each col
-        for i in range(len(self.cols)):
-            colLens.append(max([len(row[i]) for row in self.rows] + [len(self.cols[i])]))
+        # find max cell width per column
+        colWidths = []
+        for i in range(self.colNo):
+            widths = []
+            for row in self.rows:
+                widths.append(len(str(row[i])))
+            widths.append(len(str(self.cols[i])))
+            colWidths.append(max(widths))
 
         # create formattings
-        border = "+" + "+".join("{borderType}"*(colLen + 2) for colLen in colLens) + "+"
-        values = "|" + "|".join(" {:<%d} " % colLen for colLen in colLens) + "|"
+        border = "+" + "+".join("{borderType}"*(colWidth + 2) for colWidth in colWidths) + "+"
+        values = "|" + "|".join(" {:<%d} " % colWidth for colWidth in colWidths) + "|"
 
         print(border.format(borderType="="))
         print(values.format(*self.cols))
@@ -27,41 +31,58 @@ class Table:
             print(values.format(*row))
             print(border.format(borderType="-"))
 
+        return self
+    
 
-    def addRow(self, row):
+    def add_column(self, col: str) -> Self:
+        #TODO: give empty values for in rows
+        #TODO: add as right column (further update to insert in middle)
+        #TODO: update self.colNo value
+        return self
+
+    def remove_col(self, colIndex: int) -> Self:
+        #TODO: remove col
+        #TODO: update self.colNo value
+        return self
+
+
+    def add_row(self, row: list) -> Self:
         #TODO: sanitisation to ensure it contains valid amount of cols
         #TODO: add as bottom row (further update to insert in middle)
         #TODO: update self.rowNo value
-        pass
-
-    def addColumn(self, col: str):
-        #TODO: give empty values for in rows
-        #TODO: update self.colNo value
-        pass
-
-    def removeRow(self, rowIndex: int):
+        return self
+    
+    def remove_row(self, rowIndex: int) -> Self:
         #TODO: remove row
         #TODO: update self.rowNo value
+        return self
+
+
+    def edit_cell(self, colIndex: int, rowIndex: int, newValue) -> Self:
+        #TODO: index rows and cols, replacing value with new value
+        return self
+    
+
+    def to_string(self):
+        #TODO: print out string stating colNo, rowNo, and data
         pass
 
-    def removeCol(self, colIndex: int):
-        #TODO: remove col
-        #TODO: update self.colNo value
-        pass
+
+table = Table(['fname', 'lname', 'age'],
+              [['john', 'smith', 40],
+               ['jane', 'doe', 5],
+               ['joe', 'bloggs', 23]]).show()
 
 
-
-table = Table(['fname', 'lname'],
-              [['john', 'smith', '40'],
-               ['jane', 'doe', '5'],
-               ['joe', 'bloggs', '23']]).build()
-
-
-
+#TODO: what if data given is square (num of cols doesnt match width of rows)
 #TODO: indexing specific cells (could operator overload [])
 #TODO: selectable border lines (e.g., =, -, ~) - optional param in build()
 #TODO: change border line of exisiting table
+#TODO: set default settings of class (e.g., border) through staticmethod decorator
 #TODO: justify values in rows left/right/centre
 #TODO: different types of tables (e.g., compact has no border in between rows)
 #TODO: multiline values (i.e., wrapping)
-#TODO: table -> json
+#TODO: table from json, csv, etc.
+#TODO: table to json, csv, etc.
+#TODO: documentation :(
+#TODO: testing (even worse than doc)
