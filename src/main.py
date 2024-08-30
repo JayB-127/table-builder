efabb27b6a3c1691ee7a8199166a8b6e7a3c77c3
Jacob <jw.braidley@gmail.com>
2024-08-30 18:42:07 +0100

    put into class

--- src/main.py
+++ src/main.py
@@ -1,34 +1,45 @@
 
 
-def build_table(cols: list, rows: list):
-    # raise TypeError if not lists?
-    # handle unmatching numbers of cols to rows
+class Table:
+    def __init__(self, cols: list, rows: list):
+        self.cols = cols
+        self.rows = rows
+        self.colNo = len(cols)
+        self.rowNo = len(rows)
 
-    colLens = []
-    # append longest value length for each col
-    for i in range(len(cols)):
-        colLens.append(max([len(row[i]) for row in rows] + [len(cols[i])]))
+    def build(self):
+        # raise TypeError if not lists?
+        # handle unmatching numbers of cols to rows
 
-    # create formattings
-    border = "+" + "+".join("{borderType}"*(colLen + 2) for colLen in colLens) + "+"
-    values = "|" + "|".join(" {:<%d} " % colLen for colLen in colLens) + "|"
+        colLens = []
+        # append longest value length for each col
+        for i in range(len(self.cols)):
+            colLens.append(max([len(row[i]) for row in self.rows] + [len(self.cols[i])]))
 
-    print(border.format(borderType="="))
-    print(values.format(*cols))
-    print(border.format(borderType="="))
-    for row in rows:
-        print(values.format(*row))
-        print(border.format(borderType="-"))
+        # create formattings
+        border = "+" + "+".join("{borderType}"*(colLen + 2) for colLen in colLens) + "+"
+        values = "|" + "|".join(" {:<%d} " % colLen for colLen in colLens) + "|"
+
+        print(border.format(borderType="="))
+        print(values.format(*self.cols))
+        print(border.format(borderType="="))
+        for row in self.rows:
+            print(values.format(*row))
+            print(border.format(borderType="-"))
 
 
-build_table(['fname', 'lname', 'age'],
-            [['john', 'smith', '40'],
-             ['jane', 'doe', '5'],
-             ['joe', 'bloggs', '23']])
+
+table = Table(['fname', 'lname'],
+              [['john', 'smith', '40'],
+               ['jane', 'doe', '5'],
+               ['joe', 'bloggs', '23']]).build()
+
+
 
 #TODO: insert into table
 #TODO: delete from table
-#TODO: make border characters constants that can be dependent on "style" of table (e.g., lines can be -, =, ~, etc.)
+#TODO: selectable border lines (e.g., =, -, ~)
 #TODO: justify values in rows left/right/centre
 #TODO: different types of tables (e.g., compact has no border in between rows)
 #TODO: multiline values (i.e., wrapping)
+#TODO: table -> json
